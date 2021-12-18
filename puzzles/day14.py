27f1d2538acd083e9444e5a0fdd2ea3d23034c84
Eric Hambright <Eric.Hambright@hexagon.com>
2021-12-18 16:12:48 -0700

    day 14 part 2 in progress

--- puzzles/day14.py
+++ puzzles/day14.py
@@ -58,27 +58,56 @@ Part1
     Apply 10 steps of pair insertion to the polymer template and find the most and least common elements in the result. What do you get if you take the quantity of the most common element and subtract the quantity of the least common element?
 
 """
+import sys, copy
 pairs = {}
+lastchar = ""
+
+def stepTest(step, count):
+    print("Testing {} with {}".format(step,count))
+    if step == 1:
+        if not count == {'CH': 1, 'HH': 0, 'CB': 0, 'NH': 0, 'HB': 1, 'HC': 0, 'HN': 0, 'NN': 0, 'BH': 0, 'NC': 1, 'NB': 1, 'BN': 0, 'BB': 0, 'BC': 1, 'CC': 0, 'CN': 1}:
+            print(count)
+            print({'CH': 1, 'HH': 0, 'CB': 0, 'NH': 0, 'HB': 1, 'HC': 0, 'HN': 0, 'NN': 0, 'BH': 0, 'NC': 1, 'NB': 1, 'BN': 0, 'BB': 0, 'BC': 1, 'CC': 0, 'CN': 1})
+            return False
+        else:
+            return True
+    if step == 2:
+        if not count == {'CH': 0, 'HH': 0, 'CB': 2, 'NH': 0, 'HB': 0, 'HC': 1, 'HN': 0, 'NN': 0, 'BH': 1, 'NC': 0, 'NB': 2, 'BN': 0, 'BB': 2, 'BC': 2, 'CC': 1, 'CN': 1}:
+            return False
+        else:
+            return True
+    if step == 3:
+        if not count == {'CH': 2, 'HH': 1, 'CB': 0, 'NH': 0, 'HB': 3, 'HC': 0, 'HN': 0, 'NN': 0, 'BH': 1, 'NC': 1, 'NB': 4, 'BN': 2, 'BB': 4, 'BC': 3, 'CC': 1, 'CN': 2}:
+            print({'CH': 2, 'HH': 1, 'CB': 0, 'NH': 0, 'HB': 3, 'HC': 0, 'HN': 0, 'NN': 0, 'BH': 1, 'NC': 1, 'NB': 4, 'BN': 2, 'BB': 4, 'BC': 3, 'CC': 1, 'CN': 2})
+            return False
+        else:
+            return True
+    if step == 4:
+        if not count == {'CH': 0, 'HH': 1, 'CB': 5, 'NH': 1, 'HB': 0, 'HC': 3, 'HN': 1, 'NN': 0, 'BH': 3, 'NC': 1, 'NB': 9, 'BN': 6, 'BB': 9, 'BC': 4, 'CC': 2, 'CN': 3}: 
+            return False
+        else:
+            return True
+    return True
+
 
 def readPuzzleInput(part2=False):
     global pairs
+    global lastchar
     pairs = {}
     pattern = ""
-    with open("day14puzzleinput.txt", "r") as datafile:
+    with open("day14test.txt", "r") as datafile:
         pattern = datafile.readline().strip()
         datafile.readline() # skip blank
         for line in datafile.readlines():
             line = [x.strip() for x in line.split('->')]
-            if not part2:
-                pairs[line[0]] = line[1]
-            else:
-                pairs[(ord(line[0][0]),ord(line[0][1]))] = ord(line[1])
+            pairs[line[0]] = line[1]
+    lastchar = pattern[-1]
     if part2:
-        patternList = []
+        patternDict = {}
         for x in range(0,len(pattern)-1):
-            patternList.append(ord(pattern[x]))
-        print(patternList)
-        pattern = patternList
+            patternDict.setdefault(pattern[x]+pattern[x+1],0)
+        pattern = patternDict
+        
     return pattern
 
 def replacePattern(pattern):
@@ -90,7 +119,6 @@ def replacePattern(pattern):
 
 def part1(numtimes):
     pattern = readPuzzleInput()
-    # numtimes is leftover from the hopeful thought that i could solve part 2 with the same code LOL 
     x = 0
     while x<numtimes: 
         print("Pass number :{}".format(x+1))  
@@ -103,7 +131,53 @@ def part1(numtimes):
     least = min([count[x] for x in count.keys()])
     print("The answer is {}".format(most-least))
 
+def printx(chainCount):
+    newdict = {}
+    for x in [x for x in chainCount.keys() if chainCount[x] > 0]:
+        newdict[x] = chainCount[x]
+    print(newdict)
+
+def part2(numtimes):
+    chainCount = {}
+    for pair in pairs.keys():
+        chainCount.setdefault(pair,0)
+    zeroCount = copy.deepcopy(chainCount)
+    pattern = readPuzzleInput(True)
+    for combo in pattern.keys():
+        chainCount[combo]+=1
+    x = 0
+    while x<numtimes:
+        print("Pass number:{}".format(x+1))
+        copyCount = copy.deepcopy(zeroCount)
+        print("Copy is {}".format(copyCount))
+        for combo in chainCount.keys():
+            if chainCount[combo] > 0:
+                print("combo removed is {}".format(combo))
+                newchar = pairs[combo]
+                comb1 = combo[0]+newchar
+                comb2 = newchar+combo[1]
+                print('Combos added are {} and {}'.format(comb1,comb2))
+                copyCount[combo]-= 1
+                copyCount[comb1]+= 1
+                copyCount[comb2]+= 1
+        print("copyvcount is {}".format(copyCount))
+        print("chainCount is {}".format(chainCount))
+        for key,value in copyCount.items():
+            chainCount[key]+=value
+        x+=1
+        print("Chaincount is {}".format(chainCount))
+        if not stepTest(x,chainCount):
+            print("Test failed.")
+            sys.exit()
+        elemDict = {}
+        for key in chainCount.keys():
+            elemDict.setdefault(key[0],0)
+            elemDict[key[0]] += chainCount[key]
+        elemDict[lastchar]+=1
+        #print(elemDict)
+    #print(chainCount)
 
 if __name__=="__main__":
     print("The answer to part 1 is:")
     part1(10)
+    part2(10)
